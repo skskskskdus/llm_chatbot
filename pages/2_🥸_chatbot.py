e788a9f0bf16fb2010d41ab86bd19f00e9dcce95
skskskskdus <115733660+skskskskdus@users.noreply.github.com>
2024-06-11 22:49:24 +0900

    Update 2_🥸_chatbot.py

--- pages/2_🥸_chatbot.py
+++ pages/2_🥸_chatbot.py
@@ -86,10 +86,10 @@ if "retriever" not in st.session_state:
     print("Chunks split Done.")
     
     # 임베딩 및 벡터 데이터베이스 생성, 검색
-    embeddings = OpenAIEmbeddings()
-    vectordb = Chroma.from_documents(documents=splits, embedding=embeddings)
+    embedding = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(documents=splits,embedding=embedding)
     print("Retriever Done.")
-    st.session_state.retriever = vectordb
+    st.session_state.retriever = vectordb.invoke()
 
 # 프롬프트 템플릿 정의
 prompt = ChatPromptTemplate.from_template(
