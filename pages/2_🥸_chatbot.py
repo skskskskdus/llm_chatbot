3ef34edb0021c208c9c9d7f4c10508be73f41999
skskskskdus <115733660+skskskskdus@users.noreply.github.com>
2024-06-11 21:14:21 +0900

    Update 2_🥸_chatbot.py

--- pages/2_🥸_chatbot.py
+++ pages/2_🥸_chatbot.py
@@ -3,22 +3,20 @@ import zipfile
 import json
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain.docstore.document import Document
 from streamlit_extras.let_it_rain import rain
-from langchain_core.callbacks.base import BaseCallbackHandler
 from langchain.chains import ConversationChain
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
-
+from langchain_core.output_parsers import StrOutputParser
 import time
 from glob import glob
 
 # OpenAI API 키 설정
-OPENAI_API_KEY = "YOUR_API_KEY" # 실제 API 키를 설정하세요
+OPENAI_API_KEY = "YOUR_API_KEY_HERE"
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 # 페이지 설정
@@ -75,17 +73,16 @@ if "retriever" not in st.session_state:
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(documents)
     print("Chunks split Done.")
-    
+
     # 임베딩 및 벡터 데이터베이스 생성, 검색
     embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
-    vectordb = Chroma.from_documents(
-        documents=splits, embedding=embeddings)
+    vectordb = Chroma.from_documents(documents=splits, embedding=embeddings)
     print("Retriever Done.")
     st.session_state.retriever = vectordb.as_retriever()
 
 # 프롬프트 템플릿 정의
 prompt = ChatPromptTemplate.from_template(
-        """
+    """
     너는 진로 상담을 위한 챗봇이야. 
     기술 계열 상담 데이터를 사용해서 사용자의 질문에 답변할 수 있도록 학습되었어. 
     상담 데이터 외의 질문은 OpenAI의 모델을 사용하여 답변할 수 있도록 되어 있어.
@@ -96,10 +93,12 @@ prompt = ChatPromptTemplate.from_template(
     Question: {question}
     """
 )
+
 def format_docs(docs):
     return '\n\n'.join(doc.page_content for doc in docs)
 
-llm = ChatOpenAI(api_key=OPENAI_API_KEY,model_name="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo", temperature=0)
+
 # RAG Chain 연결
 rag_chain = (
     {'context':  st.session_state.retriever | format_docs, 'question': RunnablePassthrough()}
@@ -115,9 +114,6 @@ if 'conversation' not in st.session_state:
 # 응답 생성 함수 수정
 def generate_response(input_text):
     input_string = str(input_text)
-    # 이전 대화를 포함하는 BaseMessages 목록 생성
-    #base_messages = create_base_messages(st.session_state.conversation)
-    # RAG 체인에 전달하여 응답 생성
     response = rag_chain.invoke(input_string)
     return response
 
@@ -133,19 +129,19 @@ with st.form('Question'):
 for chat in st.session_state.conversation:
     st.write(f"**오조사마:** {chat['question']}")
     st.info(f"**집사:** {chat['response']}")
-    
+
 # 대화 저장 버튼
 if st.button("대화 저장"):
     if not os.path.exists('conversations'):
         os.makedirs('conversations')
-    
+
     file_path = os.path.join('conversations', 'conversation.py')
-    
+
     # 파일이 존재하는 경우 이전 대화 불러오기
     if os.path.exists(file_path):
         with open(file_path, "r", encoding='utf-8') as file:
             exec(file.read(), globals(), locals())
-    
+
     with open(file_path, "w", encoding='utf-8') as file:
         file.write("# 대화 기록\n")
         file.write("conversation = [\n")
@@ -183,6 +179,3 @@ def rose():
     )
 
 rose()
-
-
-
