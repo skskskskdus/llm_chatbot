24faa3b131602072c31c1929283853a549bf070b
skskskskdus <115733660+skskskskdus@users.noreply.github.com>
2024-06-12 00:05:34 +0900

    Update 2_🥸_chatbot.py

--- pages/2_🥸_chatbot.py
+++ pages/2_🥸_chatbot.py
@@ -87,7 +87,7 @@ if "retriever" not in st.session_state:
     
     # 임베딩 및 벡터 데이터베이스 생성, 검색
     embedding = OpenAIEmbeddings()
-    vectordb = chromadb.from_documents(splits,embedding)
+    vectordb = Chroma.from_documents(documents, embedding)
     print("Retriever Done.")
     st.session_state.retriever = vectordb.as_retriever()
     
