5e61447ba507abe348f9c40348bfcd3370045182
skskskskdus <115733660+skskskskdus@users.noreply.github.com>
2024-06-15 21:55:23 +0900

    Update 2_🥸_chatbot.py

--- pages/2_🥸_chatbot.py
+++ pages/2_🥸_chatbot.py
@@ -28,7 +28,7 @@ import time
 load_dotenv()
 
 # OpenAI API 키 설정
-OPENAI_API_KEY =   "sk-proj-GwrYmjDxYJGOSRQifC8WT3BlbkFJ1fRQajPQ5Qu6o5jfO9S9"
+OPENAI_API_KEY =   ""
  # 실제 API 키를 설정하세요
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
